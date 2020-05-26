37c7938f6d3717c079994a76b431edf56e9555ce
albin onheim <albin.oneheim@gmail.com>
2020-05-26 14:40:21 +0200

    kodat vidare och ärt mig mer  hur scss fungerar

--- Projektplan/index.php
+++ Projektplan/index.php
@@ -4,25 +4,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Starsida</title>
-    <link rel="stylesheet" href="/scss/style.css">
+    <link rel="stylesheet" href="./scss/main.css">
 </head>
 <body>
     <div class="kontainer">
         <header>
-            <h1>Filmtipset</h1>
+            <h1>FilmTipset</h1>
              <div class="nav">
                 <a href="#">Topplista</a>
                 <a href="#">Gengre</a>
                 <a href="#">Fördig</a>
                 <input type="text" name="sök" placeholder="sök film">
+                <a href="#"><img src="./bilder/användaricon.png" alt="användarens icon"></a>
              </div>
+             
         </header>
         <main>
             <div class="gillade">
                 <h3>Mest Gillade</h3>
+                <div class="filmer">
+
+                </div>
             </div>
-            <div class="På Bio">
+            <div class="påBio">
                 <h3>På bio</h3>
+                <div class="filmer">
+
+                </div>
             </div>
         </main>
     </div>
